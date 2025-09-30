3a9f08c1acbf3bc76adeeea5c8777a0a960384d7
jingduanyang <jingduanyang.0@bytedance.com>
2025-09-30 15:43:26 +0800

    hotspot: Fix core dump in java_lang_Throwable::get_stack_trace_elements()
    this allows most jtreg test failures to throw error and exit jvm
    normally

--- src/hotspot/share/classfile/javaClasses.cpp
+++ src/hotspot/share/classfile/javaClasses.cpp
@@ -3189,7 +3189,7 @@ oop java_lang_Throwable::get_stack_trace_element(Handle throwable, int index, TR
   // this should work as long as JVM does not embed an object into another.
   ObjArrayKlass* arrayKlass = ObjArrayKlass::cast(vmClasses::Object_klass()->array_klass(n, THREAD));
   objArrayHandle arrh(THREAD, arrayKlass->allocate(n, THREAD));
-  get_stack_trace_elements(arrh->length(), throwable, arrh, THREAD);
+  get_stack_trace_elements(arrh->length(), Handle(THREAD, backtrace(throwable())), arrh, THREAD);
   return arrh->obj_at(index);
 }
 #endif
