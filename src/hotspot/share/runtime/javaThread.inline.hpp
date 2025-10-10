5d1316e0fc320f12a095f7500132ba74b7d06bbc
jingduanyang <jingduanyang.0@bytedance.com>
2025-10-10 14:08:49 +0800

    hotspot: Fix JVM_StopThread, JVM_SuspendThread, JVM_ResumeThread
    use jvm25 implementation that was deleted in JDK-8320598, JDK-8293592

--- src/hotspot/share/runtime/javaThread.inline.hpp
+++ src/hotspot/share/runtime/javaThread.inline.hpp
@@ -42,9 +42,6 @@
 #include "runtime/orderAccess.hpp"
 #include "runtime/safepoint.hpp"
 
-#if HOTSPOT_TARGET_CLASSLIB == 8
-inline void JavaThread::set_external_suspend() { set_suspend_flag  (_external_suspend); }
-#endif
 
 inline void JavaThread::set_suspend_flag(SuspendFlags f) {
   uint32_t flags;
