c07e7d7f22ec7cae23aa551469e82031bb463d10
jingduanyang <jingduanyang.0@bytedance.com>
2026-04-02 15:47:19 +0800

    Backport: 8261449: Micro-optimize JVM_LatestUserDefinedLoader

--- src/hotspot/share/prims/jvm.cpp
+++ src/hotspot/share/prims/jvm.cpp
@@ -3144,10 +3144,14 @@ JVM_ENTRY(jobject, JVM_LatestUserDefinedLoader(JNIEnv *env))
   for (vframeStream vfst(thread); !vfst.at_end(); vfst.next()) {
 #if HOTSPOT_TARGET_CLASSLIB == 8
     // UseNewReflection
-    vfst.skip_reflection_related_frames(); // Only needed for 1.4 reflection
-    oop loader = vfst.method()->method_holder()->class_loader();
+    InstanceKlass* ik = vfst.method()->method_holder();
+    oop loader = ik->class_loader();
+    //vfst.skip_reflection_related_frames(); // Only needed for 1.4 reflection
     if (loader != nullptr && !SystemDictionary::is_ext_class_loader(Handle(THREAD, loader))) {
-      return JNIHandles::make_local(THREAD, loader);
+      if (!ik->is_subclass_of(vmClasses::reflect_MethodAccessorImpl_klass()) &&
+          !ik->is_subclass_of(vmClasses::reflect_ConstructorAccessorImpl_klass())) {
+        return JNIHandles::make_local(THREAD, loader);
+      }
     }
 #else
     InstanceKlass* ik = vfst.method()->method_holder();
