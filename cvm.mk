c80cd52f6ff686ec9b6a1a5014f297295ce08375
jingduanyang <jingduanyang.0@bytedance.com>
2026-05-15 10:39:33 +0800

    cvm/overlay: (2/2) Fix NMethod test infra
    Match makeMethodNotCompilable and NMethod whitebox API

--- cvm.mk
+++ cvm.mk
@@ -439,6 +439,7 @@ endif
 	$(call overlay_single,jdk8u,hotspot/test/compiler/7196199/Test7196199.java, $(JDK8_SRCROOT))
 	$(call overlay_single,jdk8u,hotspot/test/compiler/8004741/Test8004741.java, $(JDK8_SRCROOT))
 	$(call overlay_single,jdk8u,hotspot/test/compiler/stable/StableConfiguration.java, $(JDK8_SRCROOT))
+	$(call overlay_single,jdk8u,hotspot/test/testlibrary/whitebox/sun/hotspot/code/NMethod.java, $(JDK8_SRCROOT))
 
 -overlay-jtreg:
 	$(call overlay_single,jdk8u,test/jtreg-ext/requires/VMProps.java, $(JDK8_SRCROOT))
