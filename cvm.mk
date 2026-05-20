7bfb2908d5315eb54e1884e039e08bd5ade15321
jingduanyang.0 <jingduanyang.0@bytedance.com>
2026-05-20 15:25:45 +0800

    cvm/overlay: (2/2) Fix gc/startup_warnings tests
    Adjust the TestDefaultMaxRAMFraction.java startup warning test to match the JDK17 hotspot behavior while keeping the changes isolated under the JDK 8 overlay.
    
    - TestDefaultMaxRAMFraction.java: JDK 17 still warns for DefaultMaxRAMFraction, but the warning is emitted by the JDK17 hotspot deprecation path with different wording; update the expected text to the JDK17 hotspot message.

--- cvm.mk
+++ cvm.mk
@@ -448,6 +448,7 @@ endif
 	$(call overlay_single,jdk8u,hotspot/test/gc/arguments/TestUnrecognizedVMOptionsHandling.java, $(JDK8_SRCROOT))
 	$(call overlay_single,jdk8u,hotspot/test/gc/arguments/TestUseCompressedOopsErgoTools.java, $(JDK8_SRCROOT))
 	$(call overlay_single,jdk8u,hotspot/test/testlibrary/whitebox/sun/hotspot/WhiteBox.java, $(JDK8_SRCROOT))
+	$(call overlay_single,jdk8u,hotspot/test/gc/startup_warnings/TestDefaultMaxRAMFraction.java, $(JDK8_SRCROOT))
 
 -overlay-jtreg:
 	$(call overlay_single,jdk8u,test/jtreg-ext/requires/VMProps.java, $(JDK8_SRCROOT))
