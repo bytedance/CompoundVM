8726995be880c34975195bff214719652672af37
Lu Chuansheng <luchuansheng@bytedance.com>
2026-05-21 19:14:38 +0800

    Refined directory layout by using git worktree

--- cvm.mk
+++ cvm.mk
@@ -146,13 +146,10 @@ $(BOOTJDK8)/:
 	#cp -f $(WORKSPACE)/bin/linux-$(CVM_ARCH)/hsdis-$(ARCH_DIR).so $$(dirname $$(find $@ -name libjava.so))
 
 JDK8_JDK_SRC := $(JDK8_SRCROOT)/jdk/src
-JDK8_SRC_TAR_URL := https://github.com/openjdk/jdk8u/archive/refs/tags/jdk8u452-ga.tar.gz
-JDK8_SRC_TAR := $(notdir $(JDK8_SRC_TAR_URL))
 
 $(JDK8_JDK_SRC):
-	rm -f $(JDK8_SRC_TAR)
-	wget -nc -O $(JDK8_SRC_TAR) $(JDK8_SRC_TAR_URL)
-	[[ -d $(JDK8_SRCROOT) ]] || (mkdir -p $(JDK8_SRCROOT) && tar -xzf $(JDK8_SRC_TAR) -C $(JDK8_SRCROOT) --strip-components=1)
+	git worktree prune
+	git worktree add $(JDK8_SRCROOT) dev/cvm8_17_layout_v2_jdk8u
 
 cvm8: jdk8vm17
 
@@ -422,59 +419,31 @@ else
 	ARCH_ERROR := 1
 endif
 
--overlay-jdk8:
-	$(call overlay_single,jdk8u,jdk/test/com/sun/jdi/BreakpointWithFullGC.sh,$(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,jdk/test/com/sun/jdi/RedefineCrossEvent.java,$(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,jdk/test/java/lang/System/Versions.java,$(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,jdk/test/sun/misc/Version/Version.java,$(JDK8_SRCROOT))
-
--overlay-langtools8:
-	$(call overlay_single,jdk8u,langtools/test/tools/javac/annotations/8218152/MalformedAnnotationProcessorTests.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,langtools/test/tools/javac/6508981/TestInferBinaryName.java, $(JDK8_SRCROOT))
-
--overlay-hotspot8:
-	$(call overlay_single,jdk8u,hotspot/test/testlibrary/com/oracle/java/testlibrary/Platform.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/testlibrary/com/oracle/java/testlibrary/cli/CommandLineOptionTest.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/compiler/6859338/Test6859338.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/compiler/7196199/Test7196199.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/compiler/8004741/Test8004741.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/compiler/stable/StableConfiguration.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/testlibrary/whitebox/sun/hotspot/code/NMethod.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/gc/arguments/TestAggressiveHeap.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/gc/arguments/TestG1ConcRefinementThreads.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/gc/arguments/TestG1HeapRegionSize.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/gc/arguments/TestHeapFreeRatio.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/gc/arguments/TestInitialTenuringThreshold.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/gc/arguments/TestUnrecognizedVMOptionsHandling.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/gc/arguments/TestUseCompressedOopsErgoTools.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/testlibrary/whitebox/sun/hotspot/WhiteBox.java, $(JDK8_SRCROOT))
-	$(call overlay_single,jdk8u,hotspot/test/gc/startup_warnings/TestDefaultMaxRAMFraction.java, $(JDK8_SRCROOT))
-
 -overlay-jtreg:
-	$(call overlay_single,jdk8u,test/jtreg-ext/requires/VMProps.java, $(JDK8_SRCROOT))
+	#$(call overlay_single,jdk8u,test/jtreg-ext/requires/VMProps.java, $(JDK8_SRCROOT))
 
-test_jtreg8: -setup_jtreg8 -overlay-jdk8  -overlay-langtools8 -overlay-jtreg
+test_jtreg8: -setup_jtreg8 -overlay-jtreg
 	$(call run_jtreg8_test,$(JDK8_SRCROOT)/$(JT_REPO)/test,$(JT_TEST))
 
 test_jtreg8_cvm8: -setup_jtreg8
 	$(call run_jtreg8_test,$(CVM8_SRCROOT)/test,$(JT_TEST))
 
-test_jtreg8_jdk: -setup_jtreg8 -overlay-jdk8
+test_jtreg8_jdk: -setup_jtreg8
 	$(call run_jtreg8_test,$(JDK8_SRCROOT)/jdk/test,$(JT_TEST),$(JT_OPTS_EXCLUDE))
 
-test_jtreg8_jdk_tier1: -setup_jtreg8 -overlay-jdk8
+test_jtreg8_jdk_tier1: -setup_jtreg8
 	$(eval JT_TEST = ":jdk_tier1")
 	$(call run_jtreg8_test,$(JDK8_SRCROOT)/jdk/test,$(JT_TEST),$(JT_OPTS_EXCLUDE))
 
-test_jtreg8_jdk_core: -setup_jtreg8 -overlay-jdk8
+test_jtreg8_jdk_core: -setup_jtreg8
 	$(eval JT_TEST = ":jdk_core")
 	$(call run_jtreg8_test,$(JDK8_SRCROOT)/jdk/test,$(JT_TEST),$(JT_OPTS_EXCLUDE))
 
-test_jtreg8_hotspot8: -setup_jtreg8 -overlay-jtreg -overlay-hotspot8
+test_jtreg8_hotspot8: -setup_jtreg8 -overlay-jtreg
 	$(eval JT_REPO = hotspot)
 	$(call run_jtreg8_test,$(JDK8_SRCROOT)/$(JT_REPO)/test,$(JT_TEST),$(JT_OPTS_EXCLUDE))
 
-test_jtreg8_langtools: -setup_jtreg8 -overlay-langtools8
+test_jtreg8_langtools: -setup_jtreg8
 	$(eval JT_REPO = langtools)
 	$(call run_jtreg8_test,$(JDK8_SRCROOT)/$(JT_REPO)/test,$(JT_TEST),$(JT_OPTS_EXCLUDE))
 
